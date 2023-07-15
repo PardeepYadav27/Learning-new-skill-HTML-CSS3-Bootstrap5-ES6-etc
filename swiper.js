7ca84539ae6bf1688f3c351947fe5886a3cc8704
Pardeep Yadav <121375828+PardeepYadav27@users.noreply.github.com>
2023-07-15 11:02:36 +0530

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".carousel", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
